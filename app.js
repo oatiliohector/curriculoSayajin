0907337a79a5c8749c259322e86e524f7cd1876f
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-09 17:55:15 -0300

    chore: 🚧 decide to keep on the project as a web server project

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+require('dotenv').config()
+
 app.use(express.json());
 
 app.get('/hello', (req, res) => {
-  res.status(200).send('Hello World\n');
+  res.status(200).send('Hello World');
 });
 
 app.get('/goodbye', (req, res) => {
