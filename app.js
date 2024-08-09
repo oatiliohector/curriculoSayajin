3b38ca602970ad0cdbc5c28cebd7c6f68b86fd6d
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-09 18:05:03 -0300

    feat: ✨ build a pdf on the request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const PDFDocument = require('pdfkit');
 
 const app = express();
 
@@ -7,8 +8,17 @@ require('dotenv').config()
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.status(200).send('Hello World');
+app.post('/', (req, res) => {
+  const doc = new PDFDocument()
+  let filename = req.body.filename
+  filename = encodeURIComponent(filename) + '.pdf'
+  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
+  res.setHeader('Content-type', 'application/pdf')
+  const content = req.body.content
+  doc.y = 300
+  doc.text(content, 50, 50)
+  doc.pipe(res)
+  doc.end()
 });
 
 app.get('/goodbye', (req, res) => {
