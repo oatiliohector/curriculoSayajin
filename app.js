ddd2cf00a8cc701db9d55a1d10f3a9cf31e9993d
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-09 18:14:04 -0300

    chore: 🚧 add cors on my lambda

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const PDFDocument = require('pdfkit');
+const cors = require('cors');
 
 const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/', (req, res) => {
-  const doc = new PDFDocument()
-  let filename = req.body.filename
-  filename = encodeURIComponent(filename) + '.pdf'
-  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
-  res.setHeader('Content-type', 'application/pdf')
-  const content = req.body.content
-  doc.y = 300
-  doc.text(content, 50, 50)
-  doc.pipe(res)
-  doc.end()
+  const doc = new PDFDocument();
+  let filename = req.body.filename;
+  filename = encodeURIComponent(filename) + '.pdf';
+  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+  res.setHeader('Content-type', 'application/pdf');
+  const content = req.body.content;
+  doc.y = 300;
+  doc.text(content, 50, 50);
+  doc.pipe(res);
+  doc.end();
 });
 
 app.get('/goodbye', (req, res) => {
